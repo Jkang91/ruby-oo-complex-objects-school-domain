cc242eda56a6e34c956f35fb461766b7be28f32a
June Kang <Jkang34@gmail.com>
2020-11-17 17:17:07 -0500

    school domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+
+class School 
+    attr_accessor :name, :roster
+    
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name 
+        else 
+            @roster[grade] = [name] 
+        end
+    end
+
+    def grade(grade)
+        @roster[grade] 
+    end
+
+    def sort 
+        new_roster = {}
+        @roster.sort.each do |grade, student| 
+            new_roster[grade] = student.sort
+        end
+        new_roster
+    end
+end 
